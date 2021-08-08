209b410cf71ce7df5af426162d7a93438bfae7ef
Alibek Botashev <botashev_alibek@mail.ru>
2021-08-08 12:06:32 +0300

    feat(script.js):  fix bags

--- js/main.js
+++ js/main.js
@@ -1,26 +1,31 @@
 'use strict';
 
-
 const getNumber = function () {
   function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
   }
+
   let a = getRandomInt(1, 100);
   let guesses = 10;
+
   let isNumber = function (n) {
     return !isNaN(parseFloat(n)) && isFinite(n);
   };
 
   const checkNumber = function (guesses) {
     let n = prompt('Угадайте число от 1 до 100');
-    guesses--;
+
     if (n === null) {
       return alert('Игра окончена');
     }
-    else if (!isNumber(n)) {
+
+    if (!isNumber(n)) {
       alert('Введи число!');
       checkNumber(guesses);
+    } else {
+      guesses--;
     }
+
     if (guesses === 0) {
       let g = confirm('Нет попыток! Хотите сыграть еще?');
       if (g === true) {
@@ -35,20 +40,17 @@ const getNumber = function () {
       alert('Загаданное число меньше! ' + 'Осталось ' + guesses + ' попыток');
       checkNumber(guesses);
     }
-    else if (n < a) {
+
+    if (n < a) {
       alert('Загаданное число больше! ' + 'Осталось ' + guesses + ' попыток');
       checkNumber(guesses);
     }
-    else {
+
+    if (+n === a) {
       alert('Поздравляю, Вы угадали!!!');
     }
-    console.log(guesses);
-
-
-
   };
   checkNumber(guesses);
 };
-console.log(getNumber());
 
-
+getNumber();
